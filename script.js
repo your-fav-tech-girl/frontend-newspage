968feb1292450aa8b1488117f364ba5967d6b342
Funmilola Akanbi <funmilolaakanbi44@gmail.com>
2025-07-08 21:11:41 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+ function toggleMenu() {
+    const navbar = document.getElementById('navlinks');
+    const menuBtn = document.querySelector('.menu-btn');
+    navbar.classList.toggle('show');
+    menuBtn.classList.toggle('open');
+ }
